60369a9f083c62bfa930bc99a15661c4bd046c57
Nadeen Poorna <nadeenpoorna405@gmail.com>
2026-02-15 10:01:53 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,11 @@
 // ---------- Insert Your Data ---------- //
 
-const BOT_TOKEN = "BOT_TOKEN"; // Insert your bot token.
+const BOT_TOKEN = "7912521844:AAFO1PJb98V7asqsfg9Fj8TeKxUGomV8awY"; // Insert your bot token.
 const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
-const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
-const BOT_OWNER = 123456789; // Insert your telegram account id.
-const BOT_CHANNEL = -100123456789; // Insert your telegram channel id which the bot is admin in.
-const SIA_SECRET = "SIA_SECRET"; // Insert a powerful secret text and keep it safe.
+const BOT_SECRET = "NADEENxDEV"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
+const BOT_OWNER = 5737807853; // Insert your telegram account id.
+const BOT_CHANNEL = -1003243287441; // Insert your telegram channel id which the bot is admin in.
+const SIA_SECRET = "NADEEN"; // Insert a powerful secret text and keep it safe.
 const PUBLIC_BOT = false; // Make your bot public (only [true, false] are allowed).
 
 // ---------- Do Not Modify ---------- // 
